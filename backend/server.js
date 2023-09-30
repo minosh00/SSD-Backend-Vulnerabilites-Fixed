d75a08aac6cceb14a9942a2bd09e17cc4e7f9a01
Minosh_Imantha <86770967+minosh00@users.noreply.github.com>
2023-09-30 18:51:26 +0530

    Security Logging and Monitoring Failures  and    Broken Authentication

--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,24 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const morgan = require("morgan"); // Added morgan for request logging
+const dotenv = require("dotenv");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-
+// Middleware
+app.use(cors({ origin: "*" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-const dotenv = require("dotenv");
 dotenv.config();
+
+// Request logging middleware
+app.use(morgan("dev")); // Use "dev" format for request logging
+
 
 //import Routes 
 app.use("/user",  require("./routes/Userroutes"));
-
 app.use("/room", require("./routes/Room.Routes"));
 app.use("/employee", require("./routes/Employee.Routes"));
 app.use('/api', require('./routes/Comment.Routes'));
@@ -36,11 +35,18 @@ mongoose.connect(
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-
   .then(() => {
     console.log("Mongo DB Connected Successfully");
   })
   .catch((err) => console.log("DB Connection Failed", err));
+
+  // Implement security logging and monitoring
+// Log important security events and activities
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.path} from ${req.ip}`);
+  // You can customize this log format to capture more security-related information
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`Backend App is running on ${PORT}`);
